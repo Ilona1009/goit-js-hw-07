002c1a8ad0795d6ecb2c399b346d62d40e063da5
ilona1009 <maliarenko.ilona@gmail.com>
2023-03-13 22:06:18 +0100

    i
    addi

--- js/03-lozyload.js
+++ js/03-lozyload.js
@@ -1,7 +1,41 @@
-import { imgGallery } from './img-gallery.js';
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
-const divImg = document.querySelector('.gallery');
+const divGallery = document.querySelector('.gallery');
 
-const addSelector = imgGallery.reduce((img, acc)=> acc  + img, String )
+const addSelector = galleryItems.map(({ preview, original, description }) => {
+  return `
+  <a class="gallery__item" href="${original}">
+  <img class="lazyload gallery__image " data-src="${preview}" alt="${description}" />
+</a>`;
+})
+.join('');
+divGallery.innerHTML = addSelector;
 
-divImg.innerHTML = addSelector;
+divGallery.addEventListener('click', openModalImage)
+
+function openModalImage (event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  divGallery.addEventListener('keydown', closeModalEsc);
+
+  function closeModalEsc(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      divGallery.removeEventListener('keydown', closeModalEsc)
+      
+    }
+  }
+}
+
+    var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData : 'alt' });
+
+
+
+
+
+
